d3f8f0c7461f096b33cc444102902d18e36da1a6
Camilo <camilo.gonzalez@bailacthor.com>
2021-02-04 17:57:40 -0300

    mi segundo commit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('server works');
+const express = require('express');
+const app = express();
+
+//settings
+app.set('port', 3000);
+
+// middlewares
+
+// routes
+
+
+// static files
+
+//listening server
+app.listen(app.get('port'), () => {
+    console.log('Server on port', app.get('port'));
+});
