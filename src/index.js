73444e2736d3ea9996cdb7b885f62f3f1ff09f85
Camilo <camilo.gonzalez@bailacthor.com>
2021-02-05 11:59:54 -0300

    Se agregan las rutas y el motor ejs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
 
 //settings
 app.set('port', 3000);
-
-// middlewares
-
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 // routes
-
+app.use(require('./routes/'));
+// middlewares
 
 // static files
 
---
+++ src/routes/index.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const express = require('express');
+const router = express.Router();
+
+// routes
+router.get('/', (req, res) => {
+    res.render('index.html', { title: 'First Website' });
+});
+router.get('/contact', (req, res) => {
+    res.render('contact.html', { title: 'Contact Page' });
+});
+
+module.exports = router;
